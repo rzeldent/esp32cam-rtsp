4fbba6fc96bffdcafe57c2ae5c16b31b086dd8d9
Rene Zeldenthuis <rzeldent@outlook.com>
2024-02-15 21:59:08 +0100

    Unit tests in progress

--- lib/micro-rtsp-server/include/micro_rtsp_jpeg.h
+++ lib/micro-rtsp-server/include/micro_rtsp_jpeg.h
@@ -9,11 +9,10 @@ class micro_rtsp_jpeg
 public:
     micro_rtsp_jpeg(const uint8_t *jpeg, size_t size);
 
-protected:
-    bool decode_jpeg(uint8_t *jpg, size_t size);
-    std::tuple<const uint8_t *, size_t> find_jpeg_section(uint8_t **ptr, uint8_t *end, uint8_t flag);
-
     std::tuple<const uint8_t *, size_t> quantization_table_0_;
     std::tuple<const uint8_t *, size_t> quantization_table_1_;
     std::tuple<const uint8_t *, size_t> jpeg_data_;
+protected:
+    bool decode_jpeg(uint8_t *jpg, size_t size);
+    std::tuple<const uint8_t *, size_t> find_jpeg_section(uint8_t **ptr, uint8_t *end, uint8_t flag);
 };
